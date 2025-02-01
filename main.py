e1a0dbaf2042ec855f424ac7dd1223350cd3c8b2
Lucas Dias <diaslucp@gmail.com>
2025-02-01 16:07:18 -0500

    split off tkinter functionality to graphics.py

--- main.py
+++ main.py
@@ -1,27 +1,4 @@
-from tkinter import Tk, BOTH, Canvas
-class Window():
-    def __init__(self, width, height):
-        self.__root = Tk()
-        self.__root.title("Maze Solver")
-        self.canvas = Canvas(self.__root, width=width, height=height)
-        self.canvas.pack()
-        self.window_is_running = False
-        self.__root.protocol("WM_DELETE_WINDOW", self.close)
-    
-    
-    def redraw(self):
-        self.__root.update_idletasks()
-        self.__root.update()
-    
-
-    def wait_for_close(self):
-        self.window_is_running = True
-        while self.window_is_running:
-            self.redraw()
-    
-
-    def close(self):
-        self.window_is_running = False
+from graphics import Window
 
 def main():
     win = Window(800,600)
