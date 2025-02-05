448e85c0946bc547dec8d044ec3fd444e945810d
Lucas Dias <diaslucp@gmail.com>
2025-02-04 21:26:44 -0500

    added Cell draw_move

--- cell.py
+++ cell.py
@@ -30,3 +30,14 @@ class Cell:
         if self.has_bottom_wall:
             line = Line(Point(x1, y2), Point(x2, y2))
             self._win.draw_line(line)
+
+    def draw_move(self, to_cell, undo=False):
+        origin_point = Point((self._x1+self._x2)/2,(self._y1+self._y2)/2)
+        dest_point = Point((to_cell._x1+to_cell._x2)/2,(to_cell._y1+to_cell._y2)/2)
+        
+        if not undo:
+            fill_color = "red"
+        else:
+            fill_color = "grey"
+        line = Line(origin_point, dest_point)
+        self._win.draw_line(line, fill_color)
--- main.py
+++ main.py
@@ -7,24 +7,33 @@ def main():
     point2 = Point(200,300)
     line = Line(point1, point2)
 
-    # test line draw
-    # win.draw_line(line, "white")
+    # # test line draw
+    # # win.draw_line(line, "white")
 
-    c = Cell(win)
-    c.has_top_wall = False
-    c.draw(100, 100, 150, 150)
+    c1 = Cell(win)
+    c1.has_right_wall = False
+    c1.draw(50, 50, 100, 100)
 
-    c = Cell(win)
-    c.has_right_wall = False
-    c.draw(200, 200, 250, 250)
+    c2 = Cell(win)
+    c2.has_left_wall = False
+    c2.has_bottom_wall = False
+    c2.draw(100, 50, 150, 100)
 
-    c = Cell(win)
-    c.has_bottom_wall = False
-    c.draw(100, 300, 150, 350)
+    c1.draw_move(c2)
 
-    c = Cell(win)
-    c.has_left_wall = False
-    c.draw(200, 400, 250, 450)
+    c3 = Cell(win)
+    c3.has_top_wall = False
+    c3.has_right_wall = False
+    c3.draw(100, 100, 150, 150)
+
+    c2.draw_move(c3)
+
+    c4 = Cell(win)
+    c4.has_left_wall = False
+    c4.draw(150, 100, 200, 150)
+
+    c3.draw_move(c4, True)
+
     win.wait_for_close()
 
 main()
